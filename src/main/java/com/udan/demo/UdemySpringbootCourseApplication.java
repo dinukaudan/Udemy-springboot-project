0401f7f80f0289c36d7021e87bafee50659e12cd
dinukaudan <dinuka.udan@gmail.com>
2020-04-01 22:14:35 +0530

    first commit

---
+++ src/main/java/com/udan/demo/UdemySpringbootCourseApplication.java
@@ -0,0 +1,13 @@
+package com.udan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringbootCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringbootCourseApplication.class, args);
+	}
+
+}
